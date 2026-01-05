b399ab113691a6a4ea60120958fce51d189f00c4
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-01-05 16:52:11 +0530

    chores: add the args class schema

---
+++ src/llm/teacher/tools/args_schema.py
@@ -0,0 +1,7 @@
+class Args():
+    def __init__(self,type=str,description=None,enum=None,required=True):
+        self.type = type
+        self.description= description
+        self.enum = enum
+        self.required = required
+
