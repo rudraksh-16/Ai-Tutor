60bc459125357f2bb21095f100fc227360abd5b4
akshu-mishra <akshita@gkmit.co>
2026-01-07 18:33:52 +0530

    fix: design seperate file for user_prompt

---
+++ src/llm/planner/prompts/user_prompt.py
@@ -0,0 +1,18 @@
+USER_PROMPT="""
+            Topic:
+            {topic_title}
+
+            Learner profile:
+            {user_summary}
+
+            Full curriculum chapters (in order):
+            {all_chapters}
+
+            Current chapter:
+            {current_chapter_title}
+
+            Chapter outline:
+            {chapter_outline}
+
+            Please generate a detailed teaching plan for the current chapter.
+            """
--- tests/run_planner.py
+++ tests/run_planner.py
@@ -2,8 +2,8 @@ from src.llm.main import run_planner
 
 
 def main():
-    topic_id = "fffda8e8-a223-4056-a934-cd5f1941bd1f"
-    run_planner(topic_id)
+    TOPIC_ID = "fffda8e8-a223-4056-a934-cd5f1941bd1f"
+    run_planner(TOPIC_ID)
 
 
 if __name__ == "__main__":
