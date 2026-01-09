d9914f593b32ffa2110f73dc3a8e89dd5da7d678
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-01-09 10:40:49 +0530

    fix: update the teacher test file

--- tests/teacher_test.py
+++ tests/teacher_test.py
@@ -1,8 +1,34 @@
 from src.llm.main import run_teacher_agent
+from src.llm.utils.load_file import load_json, append_response_json
+from src.llm.teacher_agent.prompt import SYSTEM_PROMPT
+
 
 def main():
     TOPIC_ID = "5c5f9b4c-7b9b-4c65-8dd1-52e9580406cd"
-    run_teacher_agent(TOPIC_ID)
+    PATH = "src/llm/teacher_agent/chat_history.json"
+    while True:
+        chat_history = load_json(PATH)
+        if not chat_history:
+            chat_history = [
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {
+                    "role": "user",
+                    "content": "Hello, I want to start a new learning journey.",
+                },
+            ]
+            append_response_json(PATH, chat_history)
+        else:
+            user_input = input("\n[Your Response]: ")
+            user = {"role": "user", "content": user_input}
+            append_response_json(PATH, user)
+            chat_history.append(user)
+
+        output, tool_result = run_teacher_agent(TOPIC_ID, chat_history)
+        print("\n[Teacher]", output)
+        assistent = {"role": "assistant", "content": output}
+        tool_result.append(assistent)
+        append_response_json(PATH, tool_result)
+
 
 if __name__ == "__main__":
     main()
