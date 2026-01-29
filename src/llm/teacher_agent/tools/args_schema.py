fe7eb8eb3f313699c227a877c7487c229543645d
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-01-29 20:38:19 +0530

    fix: delete unnessary file

--- src/llm/teacher_agent/tools/args_schema.py
+++
@@ -1,6 +0,0 @@
-class Args:
-    def __init__(self, type=str, description=None, enum=None, required=True):
-        self.type = type
-        self.description = description
-        self.enum = enum
-        self.required = required
