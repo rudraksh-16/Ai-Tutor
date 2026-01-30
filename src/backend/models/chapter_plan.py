53a268602b4e54003f122baf206bd4cc8251b947
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-01-30 13:30:45 +0530

    fix : update the chapter plan model

--- src/backend/models/chapter_plan.py
+++ src/backend/models/chapter_plan.py
@@ -1,18 +1,24 @@
-from sqlalchemy import Column, ForeignKey, Text
+from sqlalchemy import Column, ForeignKey, Text, Integer,Enum,String
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
-
+from src.backend.enums.status import Status
 from src.backend.models.base import BaseModel
 
 
 class ChapterPlan(BaseModel):
     __tablename__ = "chapter_plans"
-
+    title = Column(String, nullable=False)
     chapter_id = Column(
         UUID(as_uuid=True),
         ForeignKey("chapters.id", ondelete="CASCADE"),
         nullable=False,
     )
+    status = Column(
+        Enum(Status, values_callable=lambda e: [member.value for member in e]),
+        nullable=False,
+        default=Status.PENDING.value,
+    )
+    sequence = Column(Integer,nullable=False)
     content = Column(Text, nullable=False)
 
     chapter = relationship("Chapter", back_populates="chapter_plan")
