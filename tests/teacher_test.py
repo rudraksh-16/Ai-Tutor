3380de6bac6ad1dd66e1cd79562a29ee7baedcb4
rudraksh-GKMIT <rudraksh@gkmit.co>
2026-02-02 16:28:01 +0530

    fix: update teacher test file

--- tests/teacher_test.py
+++ tests/teacher_test.py
@@ -1,33 +1,40 @@
 from src.llm.main import run_teacher_agent
 from src.llm.utils import load_json, append_response_json, add_message
+from src.llm.teacher_agent.prompt import USER_PROMPT
 
 
 def main():
     CHAPTER_ID = "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbba"
     PATH = "chat_history/teacher_agent/chat_history3.json"
-
+    chat_history = load_json(PATH)
     while True:
-        chat_history = load_json(PATH)
+        user_message = ""
         if chat_history:
-            print("\n")
-            user_input = input("\n[Your Response]: ").strip()
+            user_input = input("\n\n[Your Response]: ").strip()
             if user_input.lower() in ("bye", "good bye"):
                 break
-            user = {"role": "user", "content": user_input}
-            append_response_json(PATH, user)
-            chat_history.append(user)
+            user_message = user_input
+        else:
+            user_message = USER_PROMPT
 
         final_data = None
         print("\n[Teacher] ", end="", flush=True)
-        for event in run_teacher_agent(CHAPTER_ID, chat_history):
-            if event["type"] == "text":
-                print(event["data"], end="", flush=True)
-            elif event["type"] == "tool_call":
-                pass
-            elif event["type"] == "final":
-                final_data = event["data"]
-        chat_history = add_message( final_data=final_data)
-        append_response_json(PATH, chat_history)
+        try:
+            for event in run_teacher_agent(
+                chapter_id=CHAPTER_ID,
+                chat_history=chat_history,
+                user_message=user_message,
+            ):
+                if event["type"] == "text":
+                    print(event["data"], end="", flush=True)
+                elif event["type"] == "final":
+                    final_data = event["data"]
+            user_input = {"role": "user", "content": user_message}
+            append_response_json(PATH, user_input)
+            chat_history = add_message(final_data=final_data)
+            append_response_json(PATH, chat_history)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
